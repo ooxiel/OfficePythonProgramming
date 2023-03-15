3d6007bfad30130d010b77bbd7a8595d37bac84a
Lennard B <104066505+ooxiel@users.noreply.github.com>
2023-03-15 07:24:06 +0000

    changed dir

---
+++ Lectures/2023_03_15/helloworld.py
@@ -0,0 +1,9 @@
+# imported libs
+import os
+
+# clear; alterantive os.system('cls')
+os.system('clear')
+
+
+# print 'Hello World'
+print('Hello World')
--- helloworld.py
+++
@@ -1,3 +0,0 @@
-# starter program
-
-print('Hello World')
